ded328a695a4700e720f73a5da50764cfa338620
adityadhore795 <adityadhore795.1@gmail.com>
2025-09-05 19:50:28 +0530

    Update get_music.py

--- get_music.py
+++ get_music.py
@@ -2,6 +2,8 @@ import requests, random, os
 
 # Get your Pixabay API key from GitHub Secrets
 API_KEY = os.getenv("PIXABAY_API_KEY")
+if not API_KEY:
+    raise Exception("❌ PIXABAY_API_KEY not found. Did you set the GitHub secret?")
 QUERY = "ambient"
 
 url = f"https://pixabay.com/api/music/?key={API_KEY}&q={QUERY}&per_page=50"
